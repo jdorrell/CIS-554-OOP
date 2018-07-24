7920cda2ee878b2f334a36d5a20ce117f92e6aee
Jason Dorrell <j_dorrell0801@email.campbell.edu>
2018-07-24 05:39:06 -0400

    Submission

--- Homework_1/Convert.cpp
+++ Homework_1/Convert.cpp
@@ -9,15 +9,17 @@ Convert::Convert()
 
 int Convert::getBase10(int numberToConvert, int base)
 {
+  //Checks if number is already in Base10 and returns unchaged if it is
   if(base == 10)return numberToConvert;
   
   int digits = 0;
+  //bypasses check for digits if less than 10
   if (numberToConvert < 10) {
     digits = 1;
   }
 
   int placeHolder = numberToConvert;
-
+  //checks the number of digits
   while(placeHolder > 0){
     placeHolder /= 10;
     digits++;
@@ -26,19 +28,21 @@ int Convert::getBase10(int numberToConvert, int base)
   placeHolder = numberToConvert;
   int newNumber = 0;
   int count = 0;
-  
+  //converts to Base10
   while(count <= digits - 1){
+	//isolate least significant digit  
     int remainder = placeHolder%10;
-
+	//performs check forinvalid data, i.e. value of 3 in Base2
     if(remainder >= base){
       std::cout << "Digit " << count << " is an invalid value of "
 		<< remainder << " in base " << base << " : " << std::endl;
       return 0;
     }
-    
+    //calculate the value of each individual digit
     int x = std::pow(base, count);
     int digitValue = remainder * x;
     newNumber += digitValue;
+	//remove the least significant digit
     placeHolder /= 10;
     count++;
   }
--- Homework_1/jason_dorrell_hw_1.cpp
+++ Homework_1/jason_dorrell_hw_1.cpp
@@ -9,23 +9,25 @@ int main()
 {
   int number = 0;
   int base;
-  
+  //takes input and error checks
   while(number <= 0){
     std::cout << "Please enter a number greater than ZERO: ";
     std::cin >> number;
   }
   std::cout << "Please enter a base : ";
   std::cin >> base;
-
+  //instantiate Convert object
   Convert numberConverter;
 
   std::cout << "Number to covert is "
 	    << number << " in base "
 	    << base << std::endl;
-  
+  //convert to Base10
   int result = numberConverter.getBase10(number, base);
+  //Return value of 0 indicates invalid input, i.e. value of 3 in Base2
   if(result == 0){
     std::cout << "Your input was invalid" << std::endl;
+  //Display result
   }else std::cout << "The number in base 10 is "
 		  << result << std::endl;
 }
